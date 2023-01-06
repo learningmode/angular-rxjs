46e5404f9316315880a0bb220eb7861578866a66
learningmode <learningmode151@gmail.com>
2023-01-06 21:40:27 +0530

    changes earlier

--- angular-posts-ex1/src/app/services/post.service.ts
+++ angular-posts-ex1/src/app/services/post.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { IPost } from '../models/IPost';
 
 @Injectable({
@@ -11,7 +12,13 @@ export class PostService {
    }
 
    getPosts(){
-    return this.http.get<{[id:string]:IPost}>(`https://rxjs-posts-default-rtdb.firebaseio.com/posts.json`);
+    return this.http.get<{[id:string]:IPost}>(`https://rxjs-posts-default-rtdb.firebaseio.com/posts.json`).pipe(map(posts=>{
+      let postsData:IPost[]=[];
+      for(let id in posts){
+        postsData.push({...posts[id],id})
+      }
+      return postsData;
+    }));
    }
 
 }
